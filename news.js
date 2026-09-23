1277d75bbcdfb3329cc8b6cfdb5e9ad696b9cf23
LPTQ NTB <sekretariat.lptqntb@gmail.com>
2026-09-24 03:19:26 +0800

    Update berita via admin

--- news.js
+++ news.js
@@ -1,4 +1,4 @@
-/* Diterbitkan otomatis oleh Admin MY LPTQ NTB — 2026-09-23T12:42:34.194Z */
+/* Diterbitkan otomatis oleh Admin MY LPTQ NTB — 2026-09-23T19:19:23.437Z */
 window.NEWS = [
   {
     "tanggal": "2026-09-23",
