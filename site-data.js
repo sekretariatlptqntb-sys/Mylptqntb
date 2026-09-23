3d83aee00a65bf5eab049bc6b830e70e03dc070d
LPTQ NTB <sekretariat.lptqntb@gmail.com>
2026-09-23 15:36:29 +0800

    Update data situs via admin

--- site-data.js
+++ site-data.js
@@ -1,17 +1,19 @@
-/* Data situs — dikelola melalui Admin MY LPTQ NTB */
+/* Diterbitkan otomatis oleh Admin MY LPTQ NTB */
 window.SITEDATA = {
-  agenda: [],
-  ann: [],
-  ach: [],
-  gallery: [],
-  settings: {
-    email: "sekretariat.lptqntb@gmail.com",
-    address: "",
-    appUrl: "https://play.google.com/store/apps/details?id=com.mylptq.ntb",
-    ig: "https://www.instagram.com/lptqntb/",
-    fb: "https://www.facebook.com/profile.php?id=61588414983297",
-    yt: "https://www.youtube.com/channel/UCY8JX9qLg-LFE6lOzGy4X2g",
-    tt: "https://www.tiktok.com/@mylptq.ntb",
-    images: { hero: "" }
+  "agenda": [],
+  "ann": [],
+  "ach": [],
+  "gallery": [],
+  "settings": {
+    "email": "sekretariat.lptqntb@gmail.com",
+    "address": "",
+    "appUrl": "https://play.google.com/store/apps/details?id=com.mylptq.ntb",
+    "ig": "https://www.instagram.com/lptqntb/",
+    "fb": "https://www.facebook.com/profile.php?id=61588414983297",
+    "yt": "https://www.youtube.com/channel/UCY8JX9qLg-LFE6lOzGy4X2g",
+    "tt": "https://www.tiktok.com/@mylptq.ntb",
+    "images": {
+      "hero": ""
+    }
   }
 };
